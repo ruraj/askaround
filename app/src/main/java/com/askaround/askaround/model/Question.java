3229e31c116031be03b046f9bc11c7a858a64b82
Ruraj Joshi <ruraj.joshi@rts.com.np>
2016-10-02 12:08:25 -0500

    Added formatted address field in question model

--- app/src/main/java/com/askaround/askaround/model/Question.java
+++ app/src/main/java/com/askaround/askaround/model/Question.java
@@ -3,6 +3,7 @@ package com.askaround.askaround.model;
 /**
  * Created by sibi on 10/1/16.
  */
+
 import java.util.Date;
 
 /**
@@ -10,79 +11,88 @@ import java.util.Date;
  */
 public class Question {
 
-    private String title;
-    private String description;
-    private String category;
+  private String title;
+  private String description;
+  private String category;
 
-    private String username;
-    private long responseCount;
+  private String username;
+  private long responseCount;
 
-    private double lat;
-    private double lon;
+  private double lat;
+  private double lon;
+  private String formattedAddress;
 
-    private Date datePublished;
+  private Date datePublished;
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public String getCategory() {
-        return category;
-    }
+  public String getCategory() {
+    return category;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public void setCategory(String category) {
-        this.category = category;
-    }
+  public void setCategory(String category) {
+    this.category = category;
+  }
 
-    public long getResponseCount() {
-        return responseCount;
-    }
+  public long getResponseCount() {
+    return responseCount;
+  }
 
-    public void setResponseCount(long responseCount) {
-        this.responseCount = responseCount;
-    }
+  public void setResponseCount(long responseCount) {
+    this.responseCount = responseCount;
+  }
 
-    public double getLat() {
-        return lat;
-    }
+  public double getLat() {
+    return lat;
+  }
 
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
+  public void setLat(double lat) {
+    this.lat = lat;
+  }
 
-    public double getLon() {
-        return lon;
-    }
+  public double getLon() {
+    return lon;
+  }
 
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
+  public void setLon(double lon) {
+    this.lon = lon;
+  }
 
-    public Date getDatePublished() {
-        return datePublished;
-    }
+  public Date getDatePublished() {
+    return datePublished;
+  }
 
-    public void setDatePublished(Date datePublished) {
-        this.datePublished = datePublished;
-    }
+  public void setDatePublished(Date datePublished) {
+    this.datePublished = datePublished;
+  }
+
+  public void setFormattedAddress(String formattedAddress) {
+    this.formattedAddress = formattedAddress;
+  }
+
+  public String getFormattedAddress() {
+    return formattedAddress;
+  }
 }
